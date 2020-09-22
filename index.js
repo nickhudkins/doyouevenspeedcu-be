a8fa3434a305b44516af394211b51f6b052b2ea6
Nick Hudkins <nick@nickhudkins.com>
2020-09-22 18:33:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/bro", (req, res) => {
   res.send("<h1>YES</h1>");
 });
 
+app.get("/kuan", (req, res) => {
+  res.send(`<h1 style="text-align:center">NO</h1>`);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log("Hello world listening on port", port);
